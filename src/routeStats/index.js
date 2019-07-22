3503a53a7598d28c9c771b88a6d368a225f4b635
Iain McCallum <iain.mccallum@wipro.com>
2019-07-22 22:23:28 +0100

    sorting routes

--- src/routeStats/index.js
+++ src/routeStats/index.js
@@ -2,6 +2,8 @@ const filterRoutesBySummitType = require('./filterRoutesBySummitType.js');
 const routeData = require('../../data/routeData.json');
 const getMultiSummitRoutes = require('./getMultiSummitRoutes.js');
 
+const sortSingleSummitRouteData = require('./sortSingleSummitRouteData.js');
+
 const buildCompletionStatsMarkup = require('./buildCompletionStatsMarkup.js');
 const buildRouteGradeTableMarkup = require('./buildRouteGradeTableMarkup.js');
 const buildMultiSummitRouteTableMarkup = require('./buildMultiSummitRouteTableMarkup.js');
@@ -11,18 +13,23 @@ const saveMDfile = require('./saveMDfile.js');
 
 let multiSummitRoutes = getMultiSummitRoutes({routeData});
 let munroRoutes = filterRoutesBySummitType({ routeData, summitType: 'Munro' });
+let sortedMunroRoutes = sortSingleSummitRouteData({ routeData: munroRoutes });
 let corbettRoutes = filterRoutesBySummitType({ routeData, summitType: 'Corbett' });
+let sortedCorbettRoutes = sortSingleSummitRouteData({ routeData: corbettRoutes });
 let grahamRoutes = filterRoutesBySummitType({ routeData, summitType: 'Graham' });
+let sortedGrahamRoutes = sortSingleSummitRouteData({ routeData: grahamRoutes });
 let donaldRoutes = filterRoutesBySummitType({ routeData, summitType: 'Donald' });
+let sortedDonaldRoutes = sortSingleSummitRouteData({ routeData: donaldRoutes });
 let sub2000Routes = filterRoutesBySummitType({ routeData, summitType: 'Sub 2000' });
+let sortedSub2000Routes = sortSingleSummitRouteData({ routeData: sub2000Routes });
 
 saveMDfile({
   completionStatsMarkup: buildCompletionStatsMarkup(),
   routeGradeTableMarkup: buildRouteGradeTableMarkup({munroRoutes, corbettRoutes, grahamRoutes, donaldRoutes, sub2000Routes}),
   multiSummitRouteTableMd: buildMultiSummitRouteTableMarkup({ routeData: multiSummitRoutes }),
-  singleMunroRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: munroRoutes }),
-  singleCorbettRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: corbettRoutes }),
-  singleGrahamRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: grahamRoutes }),
-  singleDonaldRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: donaldRoutes }),
-  singleSub2000RouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sub2000Routes })
+  singleMunroRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sortedMunroRoutes }),
+  singleCorbettRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sortedCorbettRoutes }),
+  singleGrahamRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sortedGrahamRoutes }),
+  singleDonaldRouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sortedDonaldRoutes }),
+  singleSub2000RouteTableMd: buildSingleSummitRouteTableMarkup({ routeData: sortedSub2000Routes })
 });
---
+++ src/routeStats/sortSingleSummitRouteData.js
@@ -0,0 +1,41 @@
+const sortByTravelTime = ({routeData}) => {
+  let sortedRoutes = routeData.sort((a, b) => {
+    if (!a.directions || !b.directions || !a.directions.duration || !b.directions.duration) {
+      return 0;
+    }
+    if (a.directions.duration.value < b.directions.duration.value) {
+      return -1;
+    }
+    if (a.directions.duration.value > b.directions.duration.value) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedRoutes;
+}
+
+const sortSingleSummitRouteData = ({ routeData }) => {
+  //hike time
+
+  //grade
+  let grades = { g1: [], g2: [], g3: [], g4: [], g5: [] };
+
+  routeData.forEach((route) => {
+    grades[`g${route.Grade}`].push(route);
+  });
+
+  //smallest sort travel time
+  let sortedGrades = {
+    g1: sortByTravelTime({ routeData:grades.g1 }),
+    g2: sortByTravelTime({ routeData:grades.g2 }),
+    g3: sortByTravelTime({ routeData:grades.g3 }),
+    g4: sortByTravelTime({ routeData:grades.g4 }),
+    g5: sortByTravelTime({ routeData:grades.g5 })
+  }
+
+  let sortedRoutes = [].concat(sortedGrades.g1, sortedGrades.g2, sortedGrades.g3, sortedGrades.g4, sortedGrades.g5);
+  
+  return sortedRoutes;
+}
+
+module.exports = sortSingleSummitRouteData;
